bef31bb1be8a146c227e1eac9eb423219fd680fb
Nyraloid <45128617+Nyraloid@users.noreply.github.com>
2019-10-28 16:22:33 -0700

    Add files via upload

---
+++ KingCrimson/TimeLeap.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Chunky.NPCs;
+
+namespace Chunky.KingCrimson
+{
+    public class TimeLeap : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Skipping Time");
+            Description.SetDefault("It Just Works");
+            Main.debuff[Type] = false;
+            Main.pvpBuff[Type] = false;
+            Main.buffNoSave[Type] = true;
+            longerExpertDebuff = false;
+            canBeCleared = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<ChunkyPlayer>().kcImmune = true;
+        }
+    }
+}
