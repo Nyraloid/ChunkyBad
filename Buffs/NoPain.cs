201bd393ec20294995de685cfe8cf3d148c7afc4
Nyraloid <45128617+Nyraloid@users.noreply.github.com>
2020-05-08 00:37:26 +0200

    Add files via upload

---
+++ Buffs/NoPain.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Chunky.Buffs
+{
+    public class NoPain : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Cherry Power");
+            Description.SetDefault("Lelolelolelolelo");
+            Main.debuff[Type] = false;
+            Main.pvpBuff[Type] = false;
+            Main.buffNoSave[Type] = true;
+            longerExpertDebuff = false;
+            canBeCleared = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.immune = true;
+        }
+    }
+}
---
+++ Buffs/YesPain.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Chunky.Buffs
+{
+    public class YesPain : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Dropped Cherry");
+            Description.SetDefault("Whoops");
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = false;
+            Main.buffNoSave[Type] = true;
+            longerExpertDebuff = false;
+            canBeCleared = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<ChunkyPlayer>().CanEatCherry = false;
+        }
+    }
+}
